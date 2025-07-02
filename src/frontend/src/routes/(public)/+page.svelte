b0448a510c8b0ca314862bf70a0e77be47d60aa2
Christoph Suter <christoph@suter-burri.ch>
2025-07-02 07:37:00 +0200

    Translations

--- src/frontend/src/routes/(public)/+page.svelte
+++ src/frontend/src/routes/(public)/+page.svelte
@@ -29,20 +29,20 @@
 	<!-- end row -->
 
 	<div class="row">
-		<div class="col-sm-6 col-lg-3">
+		<div class="col-sm-6 col-lg-3 d-flex align-items-stretch">
 			<!-- Simple card -->
 			<div class="card d-block">
 				<img
 					class="card-img-top"
 					src="assets/images/tools/precipitation.png"
-					alt="Precipitation Tool"
+					alt="{$_('page.dashboard.precipitationtool')}"
 				/>
 				<div class="card-body">
-					<h3 class="card-title">Precipitation</h3>
+					<h3 class="card-title">{$_('page.dashboard.precipitation')}</h3>
 					<p class="card-text">
 						{$_('page.dashboard.precipitation_card')}
 					</p>
-					<a href="precipitation.html" class="btn btn-primary stretched-link">Start</a>
+					<a href="precipitation.html" class="btn btn-primary stretched-link">{$_('page.general.start')}</a>
 				</div>
 				<!-- end card-body-->
 			</div>
@@ -50,20 +50,20 @@
 		</div>
 		<!-- end col -->
 
-		<div class="col-sm-6 col-lg-3">
+		<div class="col-sm-6 col-lg-3 d-flex align-items-stretch">
 			<!-- Simple card -->
 			<div class="card d-block" >
 				<img
 					class="card-img-top"
 					src="assets/images/tools/hydrocalc.png"
-					alt="Card fsdaf cap"
+					alt="{$_('page.hydrocalc.nav')}"
 				/>
 				<div class="card-body">
-					<h3 class="card-title">HydroCalc</h3>
+					<h3 class="card-title">{$_('page.hydrocalc.nav')}</h3>
 					<p class="card-text">
-						Berechnen von hydrologischen Abflussmodellierungen, wie modifiziertes Fliesszeitverfahren, Kölla oder Clark-WSL.
+						{$_('page.dashboard.hydrocalcdesc')}
 					</p>
-					<a href="hydrocalc" class="btn btn-primary stretched-link">Start</a>
+					<a href="hydrocalc" class="btn btn-primary stretched-link">{$_('page.general.start')}</a>
 				</div>
 				<!-- end card-body-->
 			</div>
