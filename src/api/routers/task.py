6616b2ff9145a7669b85804ac18551eaeb939761
Christoph Suter <christoph@suter-burri.ch>
2025-09-11 11:50:12 +0200

    fix(api,frontend): Better error description, when the calculation is not working

--- src/api/routers/task.py
+++ src/api/routers/task.py
@@ -10,11 +10,19 @@ router = APIRouter(prefix="/task",
 @router.get("/{task_id}")
 def get_status(task_id):
     task_result = AsyncResult(task_id)
-    result = {
-        "task_id": task_id,
-        "task_status": task_result.status,
-        "task_result": json.dumps(task_result.result)
-    }
+    print(task_result.result)
+    try:
+        result = {
+            "task_id": task_id,
+                "task_status": task_result.status,
+                "task_result": json.dumps(task_result.result)
+            }
+    except:
+        result = {
+            "task_id": task_id,
+            "task_status": task_result.status,
+            "task_result": str(task_result.result)
+        }
     return JSONResponse(result)
 
 @router.get("/group/{task_id}")
