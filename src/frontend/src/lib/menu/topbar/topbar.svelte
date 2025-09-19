b16450bcaf8b0932bae2f963fc755424433c64b6
omar.bellprat@gmail.com <omar.bellprat@gmail.com>
2025-09-19 09:47:47 +0200

    style(Change-logos-and-look-of-the-frontend): Change to original AUGUR logo

--- src/frontend/src/lib/menu/topbar/topbar.svelte
+++ src/frontend/src/lib/menu/topbar/topbar.svelte
@@ -79,7 +79,7 @@
 							aria-expanded="false"
 						>
 							<img
-								src="{base}/assets/images/users/avatar-1.jpg"
+								src="{base}/assets/images/users/avatar-2.jpg"
 								width="32"
 								class="rounded-circle me-lg-2 d-flex"
 								alt="user-image"
